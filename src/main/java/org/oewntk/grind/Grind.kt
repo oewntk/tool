8ecaf31973cb6fb096074b4e58dde75d3a421816
1313ou <1313ou@gmail.com>
2026-05-19 17:58:42 +0200

    initial

---
+++ src/main/java/org/oewntk/grind/Grind.kt
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2021-2024. Bernard Bou.
+ */
+package org.oewntk.grind
+
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
+import org.oewntk.grind.Tracing.progress
+import org.oewntk.grind.Tracing.start
+import org.oewntk.model.ModelInfo
+import org.oewntk.yaml.`in`.FactoryPlus
+import java.io.File
+import org.oewntk.json.out.ModelConsumer as JsonModelConsumer
+import org.oewntk.ser.`in`.Factory as SerFactory
+import org.oewntk.ser.out.ModelConsumer as SerModelConsumer
+import org.oewntk.yaml.`in`.Factory as YamlFactory
+import org.oewntk.yaml.out.CoreModelConsumer as YamlModelConsumer
+
+/**
+ * Main class that generates the OEWN plus database
+ *
+ * @author Bernard Bou
+ * @see "https://sqlunet.sourceforge.net/schema.html"
+ */
+object Grind {
+
+    /**
+     * Main entry point
+     *
+     * @param args command-line arguments
+     * ```
+     * yamlDir [outputDir]
+     * ```
+     */
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val parser = ArgParser("yaml")
+
+        // Options (start with - or --)
+        // @formatter:off
+        val yaml by parser.argument(        ArgType.String,                                             description = "Input YAML dir")
+        val out by parser.argument(         ArgType.String,                                             description = "Output dir or file")
+        val yaml2 by parser.option(         ArgType.String,  shortName = "y2", fullName = "YAML2",      description = "Input YAML dir2")                 .default("yaml2")
+        val operation by parser.option(     ArgType.String,  shortName = "do", fullName = "operation",  description = "Operation")                       .default("nothing")
+        val inFormat by parser.option(      ArgType.String,  shortName = "if", fullName = "in format",  description = "In format")                       .default("yaml")
+        val inPlus by parser.option(        ArgType.Boolean, shortName = "p",  fullName = "plus",       description = "Plus input")                      .default(false)
+        val outFormat by parser.option(     ArgType.String,  shortName = "of", fullName = "out format", description = "Output format")                   .default("yaml")
+        val outInfo by parser.option(       ArgType.String,  shortName = "i",  fullName = "out info",   description = "Output info")                     .default("oewn.info")
+        val outOne by parser.option(        ArgType.Boolean, shortName = "1",  fullName = "out one",    description = "Output one file")                 .default(false)
+        val outMerge by parser.option(      ArgType.Boolean, shortName = "m",  fullName = "merge",      description = "Do not group generated entries")  .default(false)
+        val verbose by parser.option(       ArgType.Boolean, shortName = "v",  fullName = "verbose",    description = "Verbose output")                  .default(false)
+        // @formatter:on
+
+        parser.parse(args)
+        if (verbose) {
+            System.err.println("yaml: $yaml")
+            System.err.println("yaml2: $yaml2")
+            System.err.println("out: $out")
+            System.err.println("operation: $operation")
+            System.err.println("plus: $inPlus")
+            System.err.println("in format: $inFormat")
+            System.err.println("out format: $outFormat")
+            System.err.println("out merge: $outMerge")
+            System.err.println("out one: $outOne")
+        }
+        // Tracing
+        val startTime = start()
+
+        // Input
+        val input = File(yaml)
+        Tracing.psInfo.println("[Input] " + input.absolutePath)
+
+        // Input2
+        val input2 = File(yaml2)
+        Tracing.psInfo.println("[Input2] " + input2.absolutePath)
+
+        // Processing
+        Tracing.psInfo.println("[Op] $operation")
+
+        // Output
+        val outFile = File(out)
+        if (outFile.exists() && !outFile.isDirectory) {
+            outFile.delete()
+        }
+        Tracing.psInfo.println("[Output] " + outFile.absolutePath)
+
+        // Supply model
+        progress("before model is supplied,", startTime)
+        val model = if (inPlus)
+            FactoryPlus(input, input2, generated = !outMerge).get()!!
+        else when (inFormat) {
+            "ser" -> SerFactory(input).get()!!
+            "yaml" -> YamlFactory(input, input2, verbose).get()!!
+            else -> throw IllegalArgumentException("Unsupported input format")
+        }
+        //Tracing.psInfo.printf("[Model] %s%n%s%n%n", Arrays.toString(model.getSources()), model.info());
+        progress("after model is supplied,", startTime)
+
+        // Consume model
+        progress("before model is consumed,", startTime)
+
+        when (outFormat) {
+            "ser" -> SerModelConsumer(outFile).accept(model)
+            "json" -> JsonModelConsumer(outFile).accept(model)
+            "yaml" -> {
+                if (outMerge)
+                    File(outFile, "entries-generated.yaml").delete()
+                YamlModelConsumer(outFile, !outOne).accept(model)
+            }
+
+            else -> throw IllegalArgumentException("Unsupported output format")
+        }
+        progress("after model is consumed,", startTime)
+
+        // End
+        progress("total,", startTime)
+
+        // info
+        val modelInfo = model.info()
+        val modelCounts = ModelInfo.counts(model)
+        val modelInfo2 = "$modelInfo\n$modelCounts"
+        Tracing.psInfo.println(modelInfo2)
+        File(outInfo).writeText(modelInfo2)
+    }
+}
---
+++ src/main/java/org/oewntk/grind/Tracing.kt
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2024. Bernard Bou.
+ */
+package org.oewntk.grind
+
+import java.io.PrintStream
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.*
+
+/**
+ * Heap Memory utilities
+ *
+ * @author Bernard Bou
+ */
+object Tracing {
+
+    val psInfo: PrintStream = System.out
+
+    @Suppress("unused")
+    val psErr: PrintStream = System.err
+
+    private val psHeap: PrintStream = System.out
+
+    private val psTime: PrintStream = System.out
+
+    private const val TRACE_HEAP: Boolean = false
+
+    private const val TRACE_TIME: Boolean = false
+
+    fun start(): Long {
+        val startTime = System.currentTimeMillis()
+
+        // heap state
+        if (TRACE_HEAP) {
+            psHeap.println("[Memory]: " + Memory.memoryInfo("before,", Memory.Unit.M))
+            psHeap.println("[Heap]: " + Memory.heapInfo("before,", Memory.Unit.M))
+        }
+        return startTime
+    }
+
+    fun progress(message: String, startTime: Long) {
+        if (TRACE_TIME) {
+            val endTime = System.currentTimeMillis()
+            psTime.println("[Time]: " + (endTime - startTime) / 1000 + "s")
+        }
+
+        // heap state
+        if (TRACE_HEAP) {
+            psHeap.println("[Heap]: " + Memory.heapInfo(message, Memory.Unit.M))
+        }
+    }
+
+    object Memory {
+
+        private fun formatter(): DecimalFormat {
+            val formatter = NumberFormat.getInstance(Locale.US) as DecimalFormat
+            val symbols = formatter.decimalFormatSymbols
+            symbols.groupingSeparator = ' '
+            formatter.decimalFormatSymbols = symbols
+            return formatter
+        }
+
+        fun heapInfo(tag: String, u: Unit): String {
+            val max = Runtime.getRuntime().maxMemory()
+            val total = Runtime.getRuntime().totalMemory()
+            val free = Runtime.getRuntime().freeMemory()
+            var used = total - free
+            val future = max - total
+            var avail = free + future
+
+            used /= u.div
+            avail /= u.div
+
+            val formatter = formatter()
+            return "$tag used: ${formatter.format(used)}$u maxfree: ${formatter.format(avail)}$u"
+        }
+
+        fun memoryInfo(tag: String, u: Unit): String {
+            var max = Runtime.getRuntime().maxMemory()
+            var total = Runtime.getRuntime().totalMemory()
+            var free = Runtime.getRuntime().freeMemory()
+            var used = total - free
+            val future = max - total
+            var avail = free + future
+
+            max /= u.div // This will return Long.MAX_VALUE if there is no preset limit
+            total /= u.div
+            free /= u.div
+            used /= u.div
+            avail /= u.div
+
+            val formatter = formatter()
+            return String.format(
+                "%s max=%15s%s total=%10s%s used=%15s%s free=%15s%s avail=%15s%s",
+                tag,
+                formatter.format(max), u,
+                formatter.format(total), u,
+                formatter.format(used), u,
+                formatter.format(free), u,
+                formatter.format(avail), u
+            )
+        }
+
+        @Suppress("unused")
+        enum class Unit(val div: Long) {
+            U(1), K(1024 * U.div), M(1024 * K.div), G(1024 * M.div)
+        }
+    }
+}
