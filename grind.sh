bcf8045fde9c1bfb205558feea126bb83c113c7f
1313ou <1313ou@gmail.com>
2026-05-25 10:21:49 +0200

    grind scripts

--- grind.sh
+++ grind.sh
@@ -5,27 +5,23 @@
 #
 
 set -e
-#
-# IN="$1"
-# if [ -z "$1" ]; then
-# 	IN=wndb31
-# fi
-# echo "WNDB:  ${IN}" 1>&2;
-#
-# IN2="$2"
-# if [ -z "$2" ]; then
-# 	IN2=wndb2
-# fi
-# echo "WNDB2: ${IN2}" 1>&2;
-#
-# OUTFILE="$3"
-# if [ -z "$3" ]; then
-# 	OUTFILE=json/wn31.json
-# fi
-# mkdir -p $(dirname "${OUTDIR}")
-# echo "OUT:   ${OUTFILE}" 1>&2;
-#
-# opts="-pretty"
+
+if [ "$*" == "" ]; then
+  echo "
+in1          String,                                         Input dir or file
+out          String,                                         Output dir or file
+in2          String,  shortName = i2, fullName = in2,        Extra Input dir or file         default=yaml2
+operation    String,  shortName = do, fullName = operation,  Operation                       default=nothing
+inFormat     String,  shortName = if, fullName = in_format,  In format                       default=yaml
+inPlus       Boolean, shortName = p,  fullName = plus,       Plus input                      default=false
+outFormat    String,  shortName = of, fullName = out_format, Output format                   default=yaml
+outInfo      String,  shortName = i,  fullName = out_info,   Output info                     default=oewn.info
+outOne       Boolean, shortName = 1,  fullName = out_one,    Output one file                 default=false
+outMerge     Boolean, shortName = m,  fullName = merge,      Do not group generated entries  default=false
+verbose      Boolean, shortName = v,  fullName = verbose,    Verbose output                  default=false
+"
+exit 1
+fi
 
 jar=grind-2.4.0-uber.jar
 if [ ! -e "${jar}" ]; then
@@ -39,4 +35,4 @@ if [ ! -e "${jar}" ]; then
   echo "Non existing uber jar" >&2
   exit 2
   fi
-java -ea -jar "${jar}" ${opts} "${IN}" "${OUTFILE}"
+java -ea -jar "${jar}" "$*"
