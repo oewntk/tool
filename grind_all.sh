357d78001743fee888c7ef52d2ec881475500419
1313ou <1313ou@gmail.com>
2026-06-05 11:19:17 +0200

    house keeping

--- grind_all.sh
+++ grind_all.sh
@@ -8,19 +8,23 @@ source define_colors.sh
 
 if [ "$1" == "-h" ]; then
   echo "
-in1          String,                                         Input dir or file
-out          String,                                         Output dir or file
-in2          String,  shortName = i2, fullName = in2,        Extra Input dir or file         default=
-inFormat     String,  shortName = if, fullName = in_format,  In format                       default=yaml
-inPlus       Boolean, shortName = p,  fullName = plus,       Plus input                      default=false
-outFormat    String,  shortName = of, fullName = out_format, Output format                   default=yaml
-out2         String,  shortName = o2, fullName = out_info,   Extra Output dir or file        default=
-outOne       Boolean, shortName = 1,  fullName = out_one,    Output one file                 default=false
-outMerge     Boolean, shortName = m,  fullName = merge,      Do not group generated entries  default=false
-verbose      Boolean, shortName = v,  fullName = verbose,    Verbose output                  default=false
+in1                 String,                                             Input dir or file
+out                 String,                                             Output dir or file
+in2                 String,  shortName = i2, fullName = in2,            Extra Input dir or file,                                  default=
+inFormat            String,  shortName = if, fullName = in_format,      In format,                                                default=yaml
+inPlus              Boolean, shortName = p,  fullName = plus,           Plus input,                                               default=false
+outFormat           String,  shortName = of, fullName = out_format,     Output format,                                            default=yaml
+out2                String,  shortName = o2, fullName = out_info,       Extra Output dir or file,                                 default=
+outOne              Boolean, shortName = o1, fullName = out_one,        Output one file,                                          default=false
+outMerge            Boolean, shortName = m,  fullName = merge,          Do not group generated entries,                           default=false
+outSerialization    (o,d,m), shortName = os, fullName = serialization,  Serialization mode (oewn,data,model),                     default=oewn
+outYaml             (a,b,f), shortName = y,  fullName = yaml,           YAML format (auto,block,flow),                            default=auto
+outJson             (s,b,j), shortName = j,  fullName = json,           JSON method (value_wrapper,json_element,any_serializer),  default=any_serializer
+ verbose            Boolean, shortName = v,  fullName = verbose,        Verbose output,                                           default=false
 "
 exit 1
 fi
+
 
 IN_YAML=yaml
 IN_YAML2=yaml2
@@ -40,17 +44,19 @@ OUT_SQL=sql/data
 
 declare -A BY_KEY
 export BY_KEY=(
-[YAM_SER]="     -v    -if yaml -i2 ${IN_YAML2}  -of ser  -o2 ${OUT}/${OUT_SER2}                  ${IN_YAML}      ${OUT}/${OUT_SER}"
-[YAM_YAM]="     -v    -if yaml -i2 ${IN_YAML2}  -of yaml -o2 ${OUT}/${OUT_YAML}/${OUT_INFO}      ${IN_YAML}      ${OUT}/${OUT_YAML}"
-[YAM_SQL]="     -v    -if yaml -i2 ${IN_YAML2}  -of sql  -o2 ${OUT}/${OUT_SQL}/${OUT_INFO}       ${IN_YAML}      ${OUT}/${OUT_SQL}"
-[YAM_WNB]="     -v    -if yaml -i2 ${IN_YAML2}  -of wndb -o2 ${OUT}/${OUT_WNDB}/${OUT_INFO}      ${IN_YAML}      ${OUT}/${OUT_WNDB}"
-[YAM_JSN]="     -v    -if yaml -i2 ${IN_YAML2}  -of json -o2 ${OUT}/${OUT_JSON}/${OUT_INFO}      ${IN_YAML}      ${OUT}/${OUT_JSON_FILE}"
+#[YAM_SER]="       -v              -if yaml -i2 ${IN_YAML2}  -of ser  -o2 ${OUT}/${OUT_SER2}                  ${IN_YAML}      ${OUT}/${OUT_SER}"
+#[YAM_YAM]="       -v              -if yaml -i2 ${IN_YAML2}  -of yaml -o2 ${OUT}/${OUT_YAML}/${OUT_INFO}      ${IN_YAML}      ${OUT}/${OUT_YAML}"
+#[YAM_SQL]="       -v              -if yaml -i2 ${IN_YAML2}  -of sql  -o2 ${OUT}/${OUT_SQL}/${OUT_INFO}       ${IN_YAML}      ${OUT}/${OUT_SQL}"
+#[YAM_WNB]="       -v              -if yaml -i2 ${IN_YAML2}  -of wndb -o2 ${OUT}/${OUT_WNDB}/${OUT_INFO}      ${IN_YAML}      ${OUT}/${OUT_WNDB}"
+#[YAM_JSN]="       -v              -if yaml -i2 ${IN_YAML2}  -of json -o2 ${OUT}/${OUT_JSON}/${OUT_INFO}      ${IN_YAML}      ${OUT}/${OUT_JSON}"
+[YAM_JSN]="       -v      -o1      -if yaml -i2 ${IN_YAML2}  -of json -o2 ${OUT}/${OUT_JSON}1/${OUT_INFO}      ${IN_YAML}      ${OUT}/${OUT_JSON}1"
+#[YAM_JSN]="       -v      -m      -if yaml -i2 ${IN_YAML2}  -of json -o2 ${OUT}/${OUT_JSON}/${OUT_INFO}      ${IN_YAML}      ${OUT}/model_${OUT_JSON_FILE}"
 
-[PLUS_YAM_SER]="-v -p -if yaml -i2 ${IN_YAML2}  -of ser  -o2 ${OUTPLUS}/${OUT_SER2}              ${IN_YAMLPLUS}  ${OUTPLUS}/${OUT_SER}"
-[PLUS_YAM_YAM]="-v -p -if yaml -i2 ${IN_YAML2}  -of yaml -o2 ${OUTPLUS}/${OUT_YAML}/${OUT_INFO}  ${IN_YAMLPLUS}  ${OUTPLUS}/${OUT_YAML}"
-[PLUS_YAM_SQL]="-v -p -if yaml -i2 ${IN_YAML2}  -of sql  -o2 ${OUTPLUS}/${OUT_SQL}/${OUT_INFO}   ${IN_YAMLPLUS}  ${OUTPLUS}/${OUT_SQL}"
-[PLUS_YAM_WNB]="-v -p -if yaml -i2 ${IN_YAML2}  -of wndb -o2 ${OUTPLUS}/${OUT_WNDB}/${OUT_INFO}  ${IN_YAMLPLUS}  ${OUTPLUS}/${OUT_WNDB}"
-#[PLUS_YAM_JSN]="-v -p -if yaml -i2 ${IN_YAML2}  -of json -o2 ${OUTPLUS}/${OUT_JSON}/${OUT_INFO}  ${IN_YAMLPLUS}  ${OUTPLUS}/${OUT_JSON_FILE}"
+#[PLUS_YAM_SER]="  -v -p           -if yaml -i2 ${IN_YAML2}  -of ser  -o2 ${OUTPLUS}/${OUT_SER2}              ${IN_YAMLPLUS}  ${OUTPLUS}/${OUT_SER}"
+#[PLUS_YAM_YAM]="  -v -p           -if yaml -i2 ${IN_YAML2}  -of yaml -o2 ${OUTPLUS}/${OUT_YAML}/${OUT_INFO}  ${IN_YAMLPLUS}  ${OUTPLUS}/${OUT_YAML}"
+#[PLUS_YAM_SQL]="  -v -p           -if yaml -i2 ${IN_YAML2}  -of sql  -o2 ${OUTPLUS}/${OUT_SQL}/${OUT_INFO}   ${IN_YAMLPLUS}  ${OUTPLUS}/${OUT_SQL}"
+#[PLUS_YAM_WNB]="  -v -p           -if yaml -i2 ${IN_YAML2}  -of wndb -o2 ${OUTPLUS}/${OUT_WNDB}/${OUT_INFO}  ${IN_YAMLPLUS}  ${OUTPLUS}/${OUT_WNDB}"
+#[PLUS_YAM_JSN]="  -v -p           -if yaml -i2 ${IN_YAML2}  -of json -o2 ${OUTPLUS}/${OUT_JSON}/${OUT_INFO}  ${IN_YAMLPLUS}  ${OUTPLUS}/${OUT_JSON}"
 )
 export KEYS="${!BY_KEY[@]}"
 
